358db500c0950d26c432359665dba1de0b447b2f
stheer <scotttheer@gmail.com>
2020-11-18 10:59:40 +0000

    updating cron scheduler

--- app.js
+++ app.js
@@ -302,8 +302,7 @@ function syncFromDive(){
     }
 }
 
-cron.schedule("0 0 * * 3", function() {
-//cron.schedule("0 0 * * 6", function() {
+cron.schedule("0 0 * * 6", function() {
 //cron.schedule("*/3 * * * *", function() {
 	syncFromDive();
 });

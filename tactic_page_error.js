c4a882bd38c557be28a167e19221238287219412
Scott Theer <42327650+stheer@users.noreply.github.com>
2021-04-18 21:47:28 +0100

    Updating branch to current

--- tactic_page_error.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("link-to-tactic").addEventListener("click", function(){
-  window.location.href="/tactics";
-});
-
-document.getElementById("logo-notfound").addEventListener("click", function(){
-  window.location.href='https://www.nonviolenceinternational.net/';
-});
